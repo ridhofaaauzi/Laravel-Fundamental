049a123fb8d52aad1020ca84d1437028458feec4
Ridho Fauzi Grafika <ridh21142ti@student.nurulfikri.ac.id>
2024-04-23 19:28:10 +0700

    Routes Web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/list-menu/getAllData', function () {
+    return response()->json([
+        'info' => 'Data Berhasil didapatkan',
+        'data' => [
+            [
+                'makanan' => 'Soto Mie',
+                'harga' => '8000',
+            ],
+            [
+                'makanan' => 'Bakso Malang',
+                'harga' => '13000',
+            ],
+            [
+                'makanan' => 'Ketoprak',
+                'harga' => '15000',
+            ]
+        ]
+    ]);
+});
+
+Route::get('/list-menu', function () {
+    return "Ini adalah halaman yang menampilkan semua menu yang ada di Cafe Amandemy.";
+});
+
+Route::get('/list-menu/{makanan}/{harga}', function (Request $request) {
+    return "Anda memilih makanan " . $request->makanan .  " dengan harga " . $request->harga;
 });
